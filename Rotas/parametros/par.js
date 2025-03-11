b05174c5778ae766576afaef79c63ae46c05073b
William Matew <williaw.com@gmail.com>
2025-03-11 16:46:23 -0300

    Rotas-parametros

---
+++ Rotas/parametros/par.js
@@ -0,0 +1,23 @@
+const express = require ('express')
+const app = express ()
+
+app.get("/rota", function(req,res){
+    res.send("voce criou uma rota")
+})
+
+app.get("/par/:nome", function(req,res){
+    res.send("voce criou um parametro")
+
+})
+
+app.get("/t/:nome", function(req,res){
+    // aqui por exemplo eu posso ir no chrome e digitar
+    // t/william e sera escrito o nome william pois o parametro recebe esse nome 
+    res.send("<h1> ola"+ " " +req.params.nome + "</h1>" )
+// SO E POSSIVEL ENVIAR UM SEND UMA UNICA VEZ
+})
+
+
+app.listen(2030, function(){
+    console.log("servidor rodando")
+})
