20e69e462e481b347eacdd952046dbe7a60ec4b0
William Matew <williaw.com@gmail.com>
2025-03-13 15:42:41 -0300

    resumo

---
+++ SQL/manipulando.sql
@@ -0,0 +1,15 @@
+
+
+
+INSERT INTO usuarios (nome,idade,email) VALUES(
+    "marcelo",
+    20,
+    "marcelo.com"
+);
+
+SELECT * FROM usuarios WHERE idade <=  21;
+
+CREATE TABLE ex (
+    nome VARCHAR(100),
+    idade INT (10),
+);
